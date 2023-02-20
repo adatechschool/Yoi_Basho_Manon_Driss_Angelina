eaf1fcf8d05c3a6beda5e142c2b466c335525d0f
yuyu-10 <104772989+yuyu-10@users.noreply.github.com>
2023-02-20 16:00:19 +0100

    niveau 2 étape 4 terminé à 90%, le css n'est pas présent c'est très moche. Nous ne pouvons pas accéder aux catégories sans être login

--- Niveau0/formulaire.php
+++ Niveau0/formulaire.php
@@ -1,8 +1,4 @@
-<?php
-    include_once("header.php")
-?>
-
-        <div id="wrapper" >
+<div id="wrapper" >
             <main>
                 <article>
                     <h2>Poster un message</h2>
@@ -16,7 +12,7 @@
                      */
                     // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
                     // si on recoit un champs email rempli il y a une chance que ce soit un traitement
-                    $enCoursDeTraitement = isset($_POST['auteur']);
+                    $enCoursDeTraitement = isset($_SESSION['connected_id']);
                     if ($enCoursDeTraitement)
                     {
                         // on ne fait ce qui suit que si un formulaire a été soumis.
@@ -24,35 +20,43 @@
                         // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
                         echo "<pre>" . print_r($_POST, 1) . "</pre>";
                         // et complétez le code ci dessous en remplaçant les ???
-                        $authorId = $_POST['auteur'];
-                        $postContent = $_POST['message'];
+                        $authorId = $_SESSION['connected_id'];
+
                         $date = new DateTime();
 
                         //Etape 3 : Petite sécurité
                         // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
                         $authorId = intval($mysqli->real_escape_string($authorId));
-                        $postContent = $mysqli->real_escape_string($postContent);
                         //Etape 4 : construction de la requete
-                        $lInstructionSql = "INSERT INTO posts (user_id, content, created) VALUES ('".$_SESSION["connected_id"]."', '".$postContent."', '".$date->format('Y-m-d H:i:s')."')";
-                        // echo $lInstructionSql
-                        // Etape 5 : execution
-                        $ok = $mysqli->query($lInstructionSql);
-                        if ( ! $ok)
-                        {
-                            echo "Impossible d'ajouter le message: " . $mysqli->error;
-                        } else
-                        {
-                            echo "Message posté en tant que :" . $_SESSION["connected_id"];
+
+                        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['validation'])) {
+                            if (!empty($_POST['message'])) { 
+                                $postContent = $_POST['message'];
+                                $postContent = $mysqli->real_escape_string($postContent);
+                                $lInstructionSql = "INSERT INTO posts (user_id, content, created) VALUES ('".$authorId."', '".$postContent."', '".$date->format('Y-m-d H:i:s')."')";
+                                if (!$postContent) {
+                                    $errors[] = 'There is no message';
+                                } else {
+                                    $ok = $mysqli->query($lInstructionSql);
+                                    if ( ! $ok)
+                                    {
+                                        echo "Impossible d'ajouter le message: " . $mysqli->error;
+                                    } else
+                                    {
+                                        echo "Message posté en tant que :" . $_SESSION["connected_id"];
+                                    }
+                                }
+                            }
                         }
                     }
                     ?>                     
-                    <form action="usurpedpost.php" method="post">
+                    <form action="" method="post">
                         <input type='hidden' name='???' value='achanger'>
                         <dl>
-                            <!-- <dt><label for='message'>Message</label></dt> -->
+                            <dt><label for='message'>Message</label></dt>
                             <dd><textarea name='message'></textarea></dd>
                         </dl>
-                        <input type='submit'>
+                        <input type='submit' name="validation">
                     </form>               
                 </article>
             </main>
--- Niveau0/header.php
+++ Niveau0/header.php
@@ -1,5 +1,9 @@
 <?php
         session_start();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) { 
+            session_destroy();
+            header('Refresh:0');
+        }
 ?>
 <!doctype html>
 <html lang="fr">
@@ -11,20 +15,32 @@
     </head>
     <body>
         <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
+            <img src="logo-yoi-basho.png" alt="Logo de notre réseau social"/>
             <nav id="menu">
+            <?php if (isset($_SESSION['connected_id'])) { ?>
+                <form method="post">
                 <a href="news.php">Actualités</a>
                 <a href="wall.php?user_id=<?php echo $_SESSION['connected_id']?>">Mur</a>
                 <a href="feed.php?user_id=<?php echo $_SESSION['connected_id']?>">Flux</a>
                 <a href="tags.php?tag_id=1">Mots-clés</a>
+                </form>
+            <?php } ?>
+            <?php if (!isset($_SESSION['connected_id'])) { ?>
+                <a href="login.php">Login</a>
+            <?php } ?>
             </nav>
+            <?php if (isset($_SESSION['connected_id'])) { ?>
             <nav id="user">
                 <a href="#">Profil</a>
                 <ul>
                     <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id']?>">Paramètres</a></li>
                     <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id']?>">Mes suiveurs</a></li>
                     <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id']?>">Mes abonnements</a></li>
+                    <form method="post">
+                    <button type="sumbit" name="logout">Logout</button>
+                    </form>
                 </ul>
 
             </nav>
+            <?php } ?>
         </header>
